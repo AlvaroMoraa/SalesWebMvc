9a5934aca815890cfea65fe863567117befe5510
Alvaro <alvaaromora@gmail.com>
2021-05-25 13:41:09 -0300

    Segunda Migração

--- SalesWebMvc/Models/Enums/SaleStatus.cs
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 
 namespace SalesWebMvc.Models.Enums
 {
-    public class SaleStatus
+    public enum SaleStatus : int 
     {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
     }
 }
